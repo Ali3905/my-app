4f6e1de9bbefffb8be38c1060927626e6a152807
Ali3905 <muhammadali30905@gmail.com>
2023-08-24 15:03:58 +0500

    Fixes

--- pages/components/Home/MockupSection.js
+++ pages/components/Home/MockupSection.js
@@ -5,27 +5,27 @@ const MockupSection = () => {
   return (
     <div className="mockup_section">
       <div>
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
+        <Image src="/@1.jpg" width={100} height={500} />
+        <Image src="/@2.avif" width={100} height={500} />
+        <Image src="/@3.avif" width={100} height={500} />
       </div>
       <div>
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
+        <Image src="/@4.avif" width={100} height={500} />
+        <Image src="/@5.avif" width={100} height={500} />
+        <Image src="/@6.jpg" width={100} height={500} />
       </div>
       <div className="main_mockup">
         <Image src="/health.jpg" width={100} height={500} />
       </div>
       <div>
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
+        <Image src="/@7.jpeg" width={100} height={500} />
+        <Image src="/@1.jpg" width={100} height={500} />
+        <Image src="/@2.avif" width={100} height={500} />
       </div>
       <div>
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
-        <Image src="/health.jpg" width={100} height={500} />
+        <Image src="/@3.avif" width={100} height={500} />
+        <Image src="/@4.avif" width={100} height={500} />
+        <Image src="/@5.avif" width={100} height={500} />
       </div>
     </div>
   );
